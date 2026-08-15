47264ee504914247caa26be8f3044cdf3ad6b419
Niko11111 <nikolai.herrmann@me.com>
2026-08-16 00:50:22 +0200

    docs: security tip for a restricted FilaMan account behind an info button

--- src/app_config.h
+++ src/app_config.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define FW_VERSION  "v0.5.13-beta.35"
+#define FW_VERSION  "v0.5.13-beta.36"
 #define DONATION_URL "ko-fi.com/formfollowsfunction"
 
 #define BRIGHT_NORMAL_DEFAULT  255
