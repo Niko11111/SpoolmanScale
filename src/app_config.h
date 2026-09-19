e85b2cf978ab51ab485dcc4bf1a707b52225e155
Niko11111 <nikolai.herrmann@me.com>
2026-09-19 11:33:59 +0200

    chore(release): v0.8.0-beta.1

--- src/app_config.h
+++ src/app_config.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define FW_VERSION  "v0.7.4-beta.40"
+#define FW_VERSION  "v0.8.0-beta.1"
 #define DONATION_URL "ko-fi.com/formfollowsfunction"
 
 // Backlight PWM duty on GPIO45, 8 bit, straight through to LovyanGFX. Not a
